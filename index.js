2eb28e8b1a58b643f755178c85645ce4c0716ac9
Utech LLC <utechllc@Utechs-Mac-mini-2.local>
2023-02-28 12:54:03 +0400

    remoed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -16,7 +15,7 @@ mongoose.connect(process.env.DATABASE, ((err, db) => {
         console.log("mongodb connection failed!!!!")
     } else {
         console.log("mongodb connected success")
-        require("../cron")
+        require("./cron")
     }
 }))
 
@@ -30,11 +29,11 @@ app.get("/test",(req,res)=>{
     res.status(200).json("your server is running")
 })
 
-require('../api/routes/user')(app)
-require('../api/routes/uploadImage')(app)
-require('../api/routes/items')(app)
-require('../api/routes/monthly_config')(app)
-require('../api/routes/otherExpenses')(app)
+require('./api/routes/user')(app)
+require('./api/routes/uploadImage')(app)
+require('./api/routes/items')(app)
+require('./api/routes/monthly_config')(app)
+require('./api/routes/otherExpenses')(app)
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
